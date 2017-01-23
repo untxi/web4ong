8e44e6761f83b88f0cdb1da0a959917c154865d0
Samantha Arburola <samarburola@gmail.com>
2017-01-23 00:51:38 -0600

    Avance CSS

--- public/js/script.js
+++ public/js/script.js
@@ -3,7 +3,11 @@ function nav() {
     var x = document.getElementById("myTopnav");
     if (x.className === "col-12 topnav") {
         x.className += " responsive";
+      document.getElementById("myTopNav").style.background="var(--bg-light)";
+        document.getElementById("activo").style.background="var(--bg-dark)";
     } else {
         x.className = "col-12 topnav";
+        document.getElementById("myTopNav").style.background="var(--bg-dark)";
+        document.getElementById("activo").style.background="var(--bg-light)";
     }
  }
